502fb6573c4334e8fd6336a80c33b0afdedf0bcf
Flakebi <flakebi@t-online.de>
2021-02-15 23:17:15 +0100

    Use lds for aggregation - 110 MH/s

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,11 @@
 
 extern crate grt_amd as geobacter_runtime_amd;
 
+use std::io::Write;
 use std::mem::size_of;
 use std::ops::*;
-use std::time::Instant;
+use std::rc::Rc;
+use std::time::{Duration, Instant};
 
 use clap::Clap;
 use grt_amd::alloc::*;
@@ -14,10 +16,10 @@ use grt_amd::{GeobacterDeps, HsaAmdGpuAccel};
 use grt_core::context::Context;
 use lexical_core::ToLexical;
 use sha1::{Digest, Sha1};
-use tsproto_types::crypto::EccKeyPubP256;
+use tsproto_types::crypto::EccKeyPrivP256;
 
-const COUNT: usize = 1024 * 4;
-const PER_THREAD_COUNT: u64 = 10;
+const COUNT: usize = 1024 * 8;
+const PER_THREAD_COUNT: u64 = 200;
 
 const WG_SIZE: usize = 256;
 
@@ -26,16 +28,8 @@ const WG_SIZE: usize = 256;
 	about = clap::crate_description!())]
 struct Options {
 	/// The public key to compute the level.
-	///
-	/// Provide either key or uid.
 	#[clap(short, long)]
 	key: Option<String>,
-	// TODO Should not be the uid, but the public key…
-	/// The uid to compute the level.
-	///
-	/// Provide either key or uid.
-	#[clap(short, long)]
-	uid: Option<String>,
 	/// Start offset
 	#[clap(short, long, default_value = "0")]
 	offset: u64,
@@ -100,12 +94,12 @@ fn find_best_level(omega: &[u8], start: u64, count: u64) -> Entry {
 #[repr(C)] // Ensure we have a universally understood layout
 #[derive(GeobacterDeps)]
 pub struct Args {
-	copy: DeviceSignal,
+	// copy: DeviceSignal,
 	omega: *const [u8],
 	offset: u64,
 	result_level: *mut [u8],
 	result_offset: *mut [u64],
-	queue: DeviceSingleQueue,
+	queue: Rc<DeviceSingleQueue>,
 	completion: GlobalSignal,
 }
 impl Completion for Args {
@@ -117,7 +111,7 @@ impl Kernel for Args {
 	const WORKGROUP: <Self::Grid as GridDims>::Workgroup = Dim1D { x: ..WG_SIZE as _ };
 	type Queue = DeviceSingleQueue;
 
-	fn queue(&self) -> &Self::Queue { &self.queue }
+	fn queue(&self) -> &Self::Queue { self.queue.as_ref() }
 
 	/// This is the kernel that is run on the GPU
 	fn kernel(&self, vp: KVectorParams<Self>)
@@ -206,19 +200,25 @@ where F: FnOnce() -> R {
 }
 
 pub fn main() {
+	use tracing_subscriber::prelude::*;
+	use tracing_subscriber::{fmt, EnvFilter};
+
+	let fmt_layer = fmt::layer().with_target(false);
+	let filter_layer =
+		EnvFilter::try_from_default_env().or_else(|_| EnvFilter::try_new("info")).unwrap();
+
+	tracing_subscriber::registry().with(filter_layer).with(fmt_layer).init();
+
 	let options: Options = Options::parse();
 	let omega = if let Some(key) = options.key {
-		if options.uid.is_some() {
-			eprintln!("Only one of key and uid can be used.");
-			return;
-		}
-		let key = EccKeyPubP256::from_short(base64::decode(key).expect("Invalid base64 value"));
-		key.to_ts().expect("Cannot convert public key")
-	} else if let Some(uid) = options.uid {
-		uid
+		key
 	} else {
-		eprintln!("One of key and uid must be provided.");
-		return;
+		let key = EccKeyPrivP256::create().expect("Failed to create key");
+		let priv_key = key.to_ts().expect("Cannot convert private key");
+		let omega = key.to_pub().to_ts().expect("Cannot convert public key");
+		println!("Generated private key {}", priv_key);
+		println!("Generated public key {}", omega);
+		omega
 	};
 
 	let ctxt = time("create context", || Context::new().expect("create context"));
@@ -237,12 +237,18 @@ pub fn main() {
 	let mut omega_buf: LapVec<u8> =
 		time("alloc omega_buf", || LapVec::with_capacity_in(omega.len(), lap_alloc.clone()));
 	time("Fill omega_buf", || omega_buf.extend_from_slice(omega.as_bytes()));
-	let mut result_level_buf: LapVec<u8> =
-		time("alloc result_level_buf", || LapVec::with_capacity_in(COUNT / WG_SIZE, lap_alloc.clone()));
-	time("Fill result_level_buf", || result_level_buf.resize_with(COUNT / WG_SIZE, Default::default));
-	let mut result_offset_buf: LapVec<u64> =
-		time("alloc result_offset_buf", || LapVec::with_capacity_in(COUNT / WG_SIZE, lap_alloc.clone()));
-	time("Fill result_offset_buf", || result_offset_buf.resize_with(COUNT / WG_SIZE, Default::default));
+	let mut result_level_buf: LapVec<u8> = time("alloc result_level_buf", || {
+		LapVec::with_capacity_in(COUNT / WG_SIZE, lap_alloc.clone())
+	});
+	time("Fill result_level_buf", || {
+		result_level_buf.resize_with(COUNT / WG_SIZE, Default::default)
+	});
+	let mut result_offset_buf: LapVec<u64> = time("alloc result_offset_buf", || {
+		LapVec::with_capacity_in(COUNT / WG_SIZE, lap_alloc.clone())
+	});
+	time("Fill result_offset_buf", || {
+		result_offset_buf.resize_with(COUNT / WG_SIZE, Default::default)
+	});
 
 	let mut invoc: FuncModule<Args> = FuncModule::new(&accel);
 	invoc.compile_async();
@@ -270,15 +276,13 @@ pub fn main() {
 	println!("host ptr: 0x{:p}, agent ptr: 0x{:p}", omega_buf.as_ptr(), device_omega_ptr);
 
 	let async_copy_signal =
-		accel.new_device_signal(1).expect("HsaAmdGpuAccel::new_device_signal: async_copy_signal");
-	let kernel_signal =
-		GlobalSignal::new(1).expect("HsaAmdGpuAccel::new_host_signal: kernel_signal");
-	let results_signal0 =
+		accel.new_host_signal(1).expect("HsaAmdGpuAccel::new_device_signal: async_copy_signal");
+	let mut results_signal0 =
 		accel.new_host_signal(1).expect("HsaAmdGpuAccel::new_host_signal: results_signal");
-	let results_signal1 =
+	let mut results_signal1 =
 		accel.new_host_signal(1).expect("HsaAmdGpuAccel::new_host_signal: results_signal");
 
-	time("grant gpu access: `omega_buf` and `values`", || {
+	time("grant gpu access to buffers", || {
 		omega_buf.add_access(&*accel).expect("grant_agents_access");
 		result_level_buf.add_access(&*accel).expect("grant_agents_access");
 		result_offset_buf.add_access(&*accel).expect("grant_agents_access");
@@ -290,69 +294,118 @@ pub fn main() {
 			.expect("HsaAmdGpuAccel::async_copy_into");
 	}
 
+	time("cpu -> gpu async copy", || {
+		async_copy_signal.wait_for_zero(false).expect("unexpected signal status");
+	});
+
 	let group_size = invoc.group_size().expect("codegen failure");
 	let private_size = invoc.private_size().unwrap();
-	let queue =
-		accel.create_single_queue2(None, group_size, private_size).expect("HsaAmdGpuAccel::create_single_queue");
+	let queue = Rc::new(
+		accel
+			.create_single_queue2(None, group_size, private_size)
+			.expect("HsaAmdGpuAccel::create_single_queue"),
+	);
 
-	let args_pool =
+	let mut args_pool =
 		time("alloc kernargs pool", || ArgsPool::new::<Args>(&accel, 1).expect("ArgsPool::new"));
 
-	let args = Args {
-		copy: async_copy_signal,
-		offset: options.offset,
-		// TODO Omega has a known size, we can just put it into Args as a slice
-		omega: device_omega_ptr.as_ptr(),
-		result_level: device_result_level_ptr.as_ptr(),
-		result_offset: device_result_offset_ptr.as_ptr(),
-		completion: kernel_signal,
-		queue,
-	};
-
 	let grid = Dim1D { x: 0u32..COUNT as _ };
-	println!("Testing offsets {}..{}", options.offset, options.offset + COUNT as u64 * PER_THREAD_COUNT);
 
 	time("compiling", || invoc.compile().expect("codegen failed"));
 
-	let mut invoc = invoc.into_invoc(&args_pool);
+	let mut offset = options.offset;
+	let start = Instant::now();
+	let mut max = (0, 0);
 
-	let wait = time("dispatching", || unsafe {
-		invoc.unchecked_call_async(&grid, args).expect("Invoc::call_async")
-	});
+	let mut last_print = start;
+	let mut last_print_offset = offset;
+	let mut last_print_invocs = 0;
+	loop {
+		let mut invoc = invoc.invoc(&args_pool);
 
-	// specifically wait (without enqueuing another async copy) here
-	// so we can time just the dispatch.
-	time("dispatch wait", move || {
+		let kernel_signal =
+			GlobalSignal::new(1).expect("HsaAmdGpuAccel::new_host_signal: kernel_signal");
+		let args = Args {
+			offset,
+			// TODO Omega has a known size, we can just put it into Args as a slice
+			omega: device_omega_ptr.as_ptr(),
+			result_level: device_result_level_ptr.as_ptr(),
+			result_offset: device_result_offset_ptr.as_ptr(),
+			completion: kernel_signal,
+			queue: queue.clone(),
+		};
+		//println!("Testing offsets {}..{}", offset, offset + grid.x.end as u64 * PER_THREAD_COUNT);
+
+		let wait = /*time("dispatching", ||*/ unsafe {
+			invoc.unchecked_call_async(&grid, args).expect("Invoc::call_async")
+		}; //);
+
+		// specifically wait (without enqueuing another async copy) here
+		// so we can time just the dispatch.
+		//time("dispatch wait", move || {
 		wait.wait_for_zero(false).expect("wait for zero failed");
-	});
+		//});
 
-	// now copy the results back to the locked memory:
-	unsafe {
-		accel
-			.unchecked_async_copy_from(
-				&device_result_level_ptr,
-				&mut result_level_buf,
-				&(),
-				&results_signal0,
-			)
-			.expect("HsaAmdGpuAccel::async_copy_from");
-		accel
-			.unchecked_async_copy_from(
-				&device_result_offset_ptr,
-				&mut result_offset_buf,
-				&(),
-				&results_signal1,
-			)
-			.expect("HsaAmdGpuAccel::async_copy_from");
-	}
-	time("gpu -> cpu async copy", || {
+		drop(wait);
+		args_pool.wash();
+
+		// now copy the results back to the locked memory:
+		results_signal0.reset(accel, 1).expect("Failed to reset signal");
+		results_signal1.reset(accel, 1).expect("Failed to reset signal");
+		unsafe {
+			accel
+				.unchecked_async_copy_from(
+					&device_result_level_ptr,
+					&mut result_level_buf,
+					&(),
+					&results_signal0,
+				)
+				.expect("HsaAmdGpuAccel::async_copy_from");
+			accel
+				.unchecked_async_copy_from(
+					&device_result_offset_ptr,
+					&mut result_offset_buf,
+					&(),
+					&results_signal1,
+				)
+				.expect("HsaAmdGpuAccel::async_copy_from");
+		}
+		//time("gpu -> cpu async copy", || {
 		results_signal0.wait_for_zero(false).expect("unexpected signal status");
 		results_signal1.wait_for_zero(false).expect("unexpected signal status");
-	});
+		//});
 
-	let max = time("find maximum", || {
-		result_level_buf.iter().copied().zip(result_offset_buf.iter().copied())
-			.max_by_key(|(l, _)| *l).expect("Empty result buffer")
-	});
-	println!("Maximum level {} at offset {}", max.0, max.1);
+		let cur_max = //time("find maximum", || {
+			result_level_buf.iter().copied().zip(result_offset_buf.iter().copied())
+				.max_by_key(|(l, _)| *l).expect("Empty result buffer");
+		//});
+		if cur_max.0 > max.0 {
+			max = cur_max;
+		}
+		offset += grid.x.end as u64 * PER_THREAD_COUNT;
+		last_print_invocs += 1;
+
+		// Print stats
+		let now = Instant::now();
+		let dur = now.duration_since(last_print);
+		if dur > Duration::from_secs(2) {
+			let hashes = ((offset - last_print_offset) as f32 / dur.as_secs_f32()) as u32;
+			print!(
+				"\rMaximum level {} at offset {} | {} H/s ({} invocations)",
+				max.0, max.1, hashes, last_print_invocs
+			);
+			std::io::stdout().flush().expect("Failed to flush stdout");
+			last_print = now;
+			last_print_offset = offset;
+			last_print_invocs = 0;
+		}
+
+		if now.duration_since(start) > Duration::from_secs(15) {
+			println!(
+				"\nMaximum level {} at offset {}, next offset {}                    ",
+				max.0, max.1, offset
+			);
+			break;
+		}
+	}
 }
